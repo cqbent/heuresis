b2e4492bb22d356355e4a9df5dc8df98fd1679b8
cqbent <cqbent@gmail.com>
2015-02-26 11:32:24 -0500

    initial site commit

---
+++ sites/all/themes/heuresis/js/script.js
@@ -0,0 +1,36 @@
+/**
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */
+
+// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - https://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+(function ($, Drupal, window, document, undefined) {
+
+
+// To understand behaviors, see https://drupal.org/node/756722#behaviors
+Drupal.behaviors.my_custom_behavior = {
+  attach: function(context, settings) {
+
+    // Place your code here.
+    
+     /* main  menu - add action */
+    $('.region-header .main-menu .block-title').click(function() {
+        if ($('.region-header .main-menu').hasClass('menu-active')) {
+            $('.region-header .main-menu').removeClass('menu-active');
+        }
+        else {
+            $('.region-header .main-menu').addClass('menu-active');
+        }
+    });
+
+  }
+};
+
+
+})(jQuery, Drupal, this, this.document);
