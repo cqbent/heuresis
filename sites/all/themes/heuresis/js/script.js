b44bea2ee6c3a86efd54eb33fb89dfa9ef5eca9d
cqbent <cqbent@gmail.com>
2015-04-02 14:07:05 -0400

    style and config updates

--- sites/all/themes/heuresis/js/script.js
+++ sites/all/themes/heuresis/js/script.js
@@ -31,13 +31,42 @@ Drupal.behaviors.my_custom_behavior = {
     
     // product hotspot functions 
     $('.product-image .hotspot').hover(function() {
-        console.log('tis');
         if ($(this).hasClass('left')) {
             $('.product-container .ttip.left').addClass('active');
         }
         else {
             $('.product-container .ttip.right').addClass('active');
         }
+    });
+    
+    // product comparison functions
+    // set 2nd item as default active
+    $('.comparison-buttons li:nth-child(2) button').addClass('active');
+    $('.comparison-images img:nth-child(2)').addClass('active');
+    $('.comparison-table tr td:nth-child(2)').addClass('active');
+    $('.comparison-table tr th:nth-child(2)').addClass('active');
+    // on button click, make associated items active
+    $('.comparison-buttons button').click(function() {
+        $('.comparison-buttons li button').removeClass('active');
+        $('.comparison-images img').removeClass('active');
+        $('.comparison-table td').removeClass('active');
+        $('.comparison-table th').removeClass('active');
+        // display associated image; find associated table column and display
+        var nth_child = $('.comparison-buttons button').index(this) + 1;
+        $(this).addClass('active');
+        $('.comparison-images img:nth-child('+nth_child+')').addClass('active');
+        $('.comparison-table tr td:nth-child('+nth_child+')').addClass('active');
+        $('.comparison-table tr th:nth-child('+nth_child+')').addClass('active');
+        console.log(nth_child);
+    });
+    
+    // product features functions
+    $('.feature-buttons button').click(function() {
+        $('.feature-buttons button').removeClass('active');
+        $('.feature-description div').removeClass('active');
+        var f_id = $(this).attr('id');
+        $(this).addClass('active');
+        $('.feature-description .'+f_id).addClass('active');
     })
 
   }
